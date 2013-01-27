42e2ee0ed8ae550297ded583ce67ee3cc24ccfbb
Evan Weaver <evan@cloudbur.st>
2013-01-27 14:37:10 -0800

    Not convinced that subclassing should be encouraged for concrete types

--- test/model/user_test.rb
+++ test/model/user_test.rb
@@ -3,56 +3,56 @@ require File.expand_path('../../test_helper', __FILE__)
 class UserTest < ActiveModel::TestCase
   include ActiveModel::Lint::Tests
 
-  class User < Fauna::User
+  class Fauna::User
     field :pockets
   end
 
   def setup
     super
-    @model = User.new
+    @model = Fauna::User.new
     @attributes = {:name => 'Taran', :email => email, :password => password, :pockets => "Piggy treats."}
   end
 
   def test_create
-    user = User.create(@attributes)
+    user = Fauna::User.create(@attributes)
     assert_equal 'Taran', user.name
     assert user.persisted?
     assert user.ref
   end
 
   def test_save
-    user = User.new(@attributes)
+    user = Fauna::User.new(@attributes)
     user.save
     assert user.persisted?
   end
 
   def test_update
-    user = User.new(@attributes)
+    user = Fauna::User.new(@attributes)
     user.save
     user.update(:pockets => "Nothing")
     assert_equal 'Nothing', user.pockets
   end
 
   def test_find
-    user = User.create(@attributes)
-    user1 = User.find(user.ref)
+    user = Fauna::User.create(@attributes)
+    user1 = Fauna::User.find(user.ref)
     assert_equal user.ref, user1.ref
     assert user1.persisted?
   end
 
   def test_destroy
-    user = User.create(@attributes)
+    user = Fauna::User.create(@attributes)
     user.destroy
     assert user.destroyed?
   end
 
   def test_find_by_email
-    user = User.create(@attributes.merge(:email => 'test@example.com'))
-    assert_equal [user], User.find_by_email('test@example.com')
+    user = Fauna::User.create(@attributes.merge(:email => 'test@example.com'))
+    assert_equal [user], Fauna::User.find_by_email('test@example.com')
   end
 
   def test_find_by_name
-    user = User.create(@attributes.merge(:name => 'Henwen'))
-    assert_equal [user], User.find_by_name('Henwen')
+    user = Fauna::User.create(@attributes.merge(:name => 'Henwen'))
+    assert_equal [user], Fauna::User.find_by_name('Henwen')
   end
 end
