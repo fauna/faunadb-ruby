b4868ed9a7e470e803e9b428fbb91decf9353888
Evan Weaver <evan@cloudbur.st>
2013-03-07 12:53:01 -0800

    New example script

---
+++ examples/welcome.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+require "rubygems"
+require "pp"
+
+begin
+  require "fauna"
+rescue LoadError
+  puts "Please run: sudo gem install fauna"
+  exit!
+end
+
+print "Email: "
+email = gets.chomp
+
+print "Password: "
+pass = gets.chomp
+
+puts "\nConnected to Fauna Cloud Platform."
+
+puts "\nCreating a publisher key:"
+root = Fauna::Connection.new(email: email, password: pass)
+key = root.post("keys/publisher")['resource']['key']
+$fauna = Fauna::Connection.new(publisher_key: key)
+pp key
+
+puts "\nCreating classes:"
+class User < Fauna::User
+  pp self
+end
+
+class Spell < Fauna::Class
+  pp self
+end
+
+puts "\nCreating an event set."
+Fauna.schema do
+  with User do
+    event_set :spellbook
+  end
+
+  with Spell
+end
+
+Fauna::Client.context($fauna) do
+  puts "\nMigrating."
+  Fauna.migrate_schema!
+
+  puts "\nCreating a user:"
+  user = User.create!(
+                             email: "#{object_id}@example.com",
+                             password: "1234",
+                             data: {
+                               name: "Taran",
+                               profession: "Pigkeeper",
+                               location: "Caer Dallben"
+                             })
+  pp user.struct
+
+  puts "\nCreating an instance:"
+  spell = Spell.create!(
+                       data: {
+                         pronouncement: "Draw Dyrnwyn only thou of royal blood.",
+                         title: "Protector of Dyrnwyn"
+                        })
+  pp spell.struct
+
+  puts "\nAdding the instance to the user's event set."
+  user.spellbook.add(spell)
+
+  puts "\nFetching the event set:"
+  pp user.spellbook.page.struct
+end
