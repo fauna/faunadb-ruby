7c114b50d446787414620ddb751928cb186db1bb
Andrew Hampe <andrew@faunadb.com>
2015-07-10 16:34:58 -0700

    Remove unused files

--- lib/fauna.rb
+++ lib/fauna.rb
@@ -7,18 +7,6 @@ require 'zlib'
 
 load "#{File.dirname(__FILE__)}/tasks/fauna.rake" if defined?(Rake)
 
-module Fauna
-  class Invalid < RuntimeError
-  end
-
-  class NotFound < RuntimeError
-  end
-end
-
 require 'fauna/util'
 require 'fauna/connection'
-require 'fauna/cache'
 require 'fauna/client'
-require 'fauna/resource'
-require 'fauna/named_resource'
-require 'fauna/set'
